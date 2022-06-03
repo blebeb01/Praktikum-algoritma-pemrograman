7cbeb70be0c09e4049012bc6b808dd9f7c6ac2e5
Muhammad Naufal Muthahhari <95490494+blebeb01@users.noreply.github.com>
2022-06-03 13:59:15 +0700

    Add files via upload

---
+++ struct.cpp
@@ -0,0 +1,23 @@
+include <iostream>
+using namespace std;
+
+struct hero {
+	string nama;
+	int level;
+	string role;
+	char tier;
+	int atk;
+	int def;
+};
+
+int main() {
+	hero hero1;
+	hero1.nama = "Amogus";
+	hero1.role = "Impostor";
+	hero1.tier = 'S';
+	hero1.level = 69;
+	hero1.atk = 420;
+	hero1.def = 1;
+	
+	cout << "Name:	" << hero1.nama << "\n" << "Tier:	" << hero1.tier 
+}
