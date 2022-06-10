2fa401f6dcaf70281991a6143d8c594615a0ba79
Muhammad Naufal Muthahhari <95490494+blebeb01@users.noreply.github.com>
2022-06-10 14:35:04 +0700

    Add files via upload

---
+++ func1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+void nilaiAkhir(int nilai) {
+	string nilaiString = "E"
+	if(nilai >= 75){
+		nilaiString = "A";
+	} else if(nilai >= 65 && nilai <= 74){
+		
+	}
+	}
+}
+int main() {
+	
+}
